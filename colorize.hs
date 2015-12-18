446f0384a8881ee3135fb903a584a4e0d641b149
Akos Fabian <fabian.akos@gmail.com>
2015-12-18 17:24:57 +0100

    Also colorizing words targeted for learning

--- colorize.hs
+++ colorize.hs
@@ -13,8 +13,10 @@ processInputFile :: FilePath -> IO ()
 processInputFile inputFile = do
         inputText <- readFile inputFile
         words1kText <- readFile "cocadb/basewrd01.txt"
+        words5kText <- readFile "cocadb/basewrd05.txt"
         let knownWordSet = Set.fromList (words words1kText)
-        let colorizedLines = map (linify . process knownWordSet) (lines inputText)
+        let targetWordSet = Set.fromList (words words5kText)
+        let colorizedLines = map (linify . process knownWordSet targetWordSet) (lines inputText)
         putStrLn $ htmlize (unlines colorizedLines)
 
 linify :: String -> String
@@ -29,9 +31,6 @@ colorForTargetWords = "LightGreen"
 colorForUnknownWords :: String
 colorForUnknownWords = "DarkSalmon"
 
-wordsTargeted :: [String]
-wordsTargeted = ["fall", "beach"]
-
 popWord :: String -> String
 popWord = takeWhile isAlpha
 
@@ -41,19 +40,22 @@ skipWord = dropWhile isAlpha
 copyTillWord :: String -> String
 copyTillWord = takeWhile (not . isAlpha)
 
-process :: Set.Set String -> String -> String
-process _ [] = []
-process knownWordSet xs = colorize headWord knownWordSet ++ nonWordAfterHead ++ process knownWordSet tailWords
+process :: Set.Set String -> Set.Set String -> String -> String
+process _ _ [] = []
+process knownWordSet targetWordSet xs =
+        colorize headWord knownWordSet targetWordSet ++
+        nonWordAfterHead ++
+        process knownWordSet targetWordSet tailWords
         where
                 headWord = popWord xs
                 nonWordAfterHead = copyTillWord $ skipWord xs
                 tailWords = drop (length headWord + length nonWordAfterHead) xs
 
-colorize :: String -> Set.Set String -> String
-colorize [] _ = []
-colorize xs knownWordSet
+colorize :: String -> Set.Set String -> Set.Set String -> String
+colorize [] _ _ = []
+colorize xs knownWordSet targetWordSet
         | map toUpper xs `elem` knownWordSet = xs
-        | xs `elem` wordsTargeted = decorate xs colorForTargetWords
+        | map toUpper xs `elem` targetWordSet = decorate xs colorForTargetWords
         | otherwise = decorate xs colorForUnknownWords
 
 decorate :: String -> String -> String
