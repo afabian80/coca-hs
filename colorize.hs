c559e23efc04862e3d07d96bcfee62ded32bc562
Akos Fabian <fabian.akos@gmail.com>
2015-12-18 10:29:21 +0100

    Sample string variable colorized

---
+++ colorize.hs
@@ -0,0 +1,51 @@
+import Data.Char (isAlpha)
+
+main :: IO ()
+main = do
+        let colorizedText = process subtitle
+        putStrLn $ htmlize colorizedText
+
+htmlize :: String -> String
+htmlize xs = "<html>\n<body>\n" ++ xs ++ "\n</body>\n</html>"
+
+colorForTargetWords :: String
+colorForTargetWords = "LightGreen"
+
+colorForUnknownWords :: String
+colorForUnknownWords = "DarkSalmon"
+
+subtitle :: String
+subtitle = "2\n00:06:26,193 --> 00:06:28,926\nI'll have a smoke outside,\nor I'll fall asleep.\n\"Son of a beach\" - said Kolya."
+
+wordsKnown :: [String]
+wordsKnown = ["one", "two", "have", "smoke", "outside", "said"]
+
+wordsTargeted :: [String]
+wordsTargeted = ["fall", "beach"]
+
+popWord :: String -> String
+popWord = takeWhile isAlpha
+
+skipWord :: String -> String
+skipWord = dropWhile isAlpha
+
+copyTillWord :: String -> String
+copyTillWord = takeWhile (not . isAlpha)
+
+process :: String -> String
+process [] = []
+process xs = colorize headWord ++ nonWordAfterHead ++ process tailWords
+        where
+                headWord = popWord xs
+                nonWordAfterHead = copyTillWord $ skipWord xs
+                tailWords = drop (length headWord + length nonWordAfterHead) xs
+
+colorize :: String -> String
+colorize [] = []
+colorize xs
+        | xs `elem` wordsKnown = xs
+        | xs `elem` wordsTargeted = decorate xs colorForTargetWords
+        | otherwise = decorate xs colorForUnknownWords
+
+decorate :: String -> String -> String
+decorate xs tag = "<span style=\"background-color:" ++ tag ++ "\">" ++ xs ++ "</span>"
