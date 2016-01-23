991b0fcbf17e5815e08409871785eae614eb3a7b
Akos Fabian <fabian.akos@gmail.com>
2016-01-23 17:19:19 +0100

    Experimental decoration only in html body

--- html-replace.hs
+++ html-replace.hs
@@ -1,4 +1,5 @@
 import Data.Char (isAlpha)
+import Data.List (isPrefixOf)
 
 main :: IO ()
 main = interact colorize
@@ -10,7 +11,7 @@ sampleTagged :: String
 sampleTagged = "Egy cica, <b>ket</b> cica, harom cica, hajj. Megfogta az icipici egeret!"
 
 sampleHtml :: String
-sampleHtml = "<html><head>Itt nem kene semmit cserelni!</head><body>Egy cica, <b>ket</b> cica, harom cica, hajj. Megfogta az icipici egeret!</body></html>"
+sampleHtml = "<html><head>Itt nem kene semmit cserelni!</head><body>Egy cica, <bold>ket</bold> cica, harom cica, hajj. Megfogta az icipici egeret!</body></html>"
 
 colorize :: String -> String
 colorize = colorizeRecurse False
@@ -18,9 +19,11 @@ colorize = colorizeRecurse False
 colorizeRecurse :: Bool -> String -> String
 colorizeRecurse _ [] = []
 colorizeRecurse doChange text@(x:xs)
-        | isAlpha x = decorate (takeWhile isAlpha text) ++ colorizeRecurse doChange (dropWhile isAlpha text)
-        | x == '<' = x : takeWhile (/= '>') xs ++ colorizeRecurse doChange (dropWhile (/= '>') xs)
+        | isAlpha x = decorate doChange (takeWhile isAlpha text) ++ colorizeRecurse doChange (dropWhile isAlpha text)
+        | x == '<' = x : takeWhile (/= '>') xs ++ colorizeRecurse updatedDoChange (dropWhile (/= '>') xs)
         | otherwise = x : colorizeRecurse doChange xs
+        where
+                updatedDoChange = doChange || "body " `isPrefixOf` xs || "body>" `isPrefixOf` xs
 
-decorate :: String -> String
-decorate text = "(" ++ text ++ ")"
+decorate :: Bool -> String -> String
+decorate doChange text = if doChange then "(" ++ text ++ ")" else text
