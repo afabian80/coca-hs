a7f65c420eb919d28a9367b0ee4534e0ea536ef4
Akos Fabian <fabian.akos@gmail.com>
2016-07-18 20:22:31 +0200

    Add progress report to translate script

--- translate.sh
+++ translate.sh
@@ -12,9 +12,13 @@ fi
 
 echo "Translating words..."
 
+iter=0
+NUM=$(wc -l < $DICT | tr -d ' ')
 # sample dictionary line:
 # SQUIRE 116 (squire squires ) =fegyverhordozó
 while read line; do
+    let "iter++"
+    echo -ne "\r${iter}/${NUM}"
     # get the text in the bracket
     WORDS=$(echo $line | cut -d '(' -f2 | cut -d ')' -f1)
     # get translation
@@ -22,7 +26,6 @@ while read line; do
     # get occurances
     OCC=$(echo $line | cut -d ' ' -f2)
     for word in $WORDS; do
-        echo "Translating $word"
         # using GNU sed command (for case insensitive replace)
         sed -i "s/\b\($word\)\b/\1 ($TRANSLATION $OCC)/gI" $INPUT
     done
